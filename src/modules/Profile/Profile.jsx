fc3593c890093de77353b4db61fed36e7282575a
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-06-29 10:39:10 +0100

    Fix typo

--- src/modules/Profile/Profile.jsx
+++ src/modules/Profile/Profile.jsx
@@ -45,7 +45,7 @@ const ProfileContent = ({
         </div>
         <div className={styles.description}>
           <span className={styles.heading}>Description</span>
-          <span className="wrapper">{desc || 'None available'}</span>
+          <span className="wrapper">{desc || 'Not available'}</span>
         </div>
         <div className={styles.chat}>
           <ReactImageFallback
