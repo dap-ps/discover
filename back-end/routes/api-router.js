8f0e567f736a11dbde356cf1b78eb8c485c25afa
Jakub Sokołowski <jakub@status.im>
2019-07-29 17:02:52 -0400

    add a /healthcheck endpoint
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- back-end/routes/api-router.js
+++ back-end/routes/api-router.js
@@ -5,6 +5,9 @@ const DAppsRoute = require('./dapps-routes');
 class APIRouter {
     static route(app) {
         app.use('/metadata', DAppsRoute.build(express));
+
+        /* for ElasticBeanstalk Load Balancer healthcheck */
+        app.use('/healthcheck', async (req, res) => res.send('OK'))
     }
 }
 
