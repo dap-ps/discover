02688fd2a0d6eac97e59127721141a3d9933f1ae
andytudhope <tuddy0525@gmail.com>
2019-07-30 11:37:34 +0200

    Turn off embark pipeline

--- config/pipeline.js
+++ config/pipeline.js
@@ -20,7 +20,7 @@ module.exports = {
   //   https://github.com/embark-framework/embark-typescript-template
   // A new DApp can be created from that template with:
   //   embark new --template typescript
-  enabled: true 
+  enabled: false 
   // Setting `enabled: false` in this config will disable Embark's built-in Webpack
   // pipeline. The developer will need to use a different frontend build tool, such as 
   // `create-react-app` or Angular CLI to build their dapp
