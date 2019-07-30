f8c03141ad8ff66651dc1fd8557fd1d0e42d9b8b
andytudhope <tuddy0525@gmail.com>
2019-07-30 14:10:22 +0200

    Updates to correct contracts & readme

--- config/contracts.js
+++ config/contracts.js
@@ -74,9 +74,9 @@ module.exports = {
       SafeMath: { deploy: false },
       TestBancorFormula: { deploy: false },
       MiniMeToken: {
-        address: '0x2764b5da3696E3613Ef9864E9B4613f9fA478E75',
+        address: '0x25B1bD06fBfC2CbDbFc174e10f1B78b1c91cc77B',
       },
-      Discover: { address: '0xc13711209ba3d5b4cbbcb45f7f64643abb267dbd' },
+      Discover: { address: '0x17e7a7330d23fc6a2ab8578a627408f815396662' },
       // MiniMeToken: {
       //   args: [
       //     '$MiniMeTokenFactory',
