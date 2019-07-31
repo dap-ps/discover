8550d562025517bde2c16a1a1393d10a189f39a8
andytudhope <tuddy0525@gmail.com>
2019-07-31 14:56:00 +0200

    Updated Discover instance with STT on ropsten

--- config/contracts.js
+++ config/contracts.js
@@ -125,7 +125,7 @@ module.exports = {
         address: '0xc55cf4b03948d7ebc8b9e8bad92643703811d162',
       },
       Discover: {
-        args: ['$MiniMeToken'],
+        address: '0x9fb115BC152aE21ECDc81c73843673fa38e4D2dB',
       },
     },
     tracking: 'shared.testnet.chains.json',
