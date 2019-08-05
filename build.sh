fc80ae85dc772e1a41adfa2e951d8d26e07cff55
Jakub Sokołowski <jakub@status.im>
2019-08-05 09:51:31 -0400

    Revert "Updates build process to use env for setting endpoints"
    This reverts commit a92592c223a5f09abeb3cc444ecc528d28a1d9f1.

--- build.sh
+++ build.sh
@@ -4,12 +4,7 @@ echo "creating new full-build"
 mkdir full-build
 cp -r back-end/* full-build/
 echo "building new frontend"
-if [ $1 = "--dev" ]
-then 
-    npm run build-dev
-else
-    npm run build
-fi
+npm run build
 echo "copying new frontend"
 rm -rf full-build/frontend/*
 cp -r build/* full-build/frontend/
