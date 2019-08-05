11ba011c7a418547784cd1717e0bac2425d0747a
Jakub Sokołowski <jakub@status.im>
2019-08-05 10:11:59 -0400

    fix node dependencies in package.json
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "creating new full-build"
 mkdir full-build
 cp -r back-end/* full-build/
 echo "building new frontend"
-npm run build
+yarn run build
 echo "copying new frontend"
 rm -rf full-build/frontend/*
 cp -r build/* full-build/frontend/
