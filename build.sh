3d2d1660cab49e3b4847ef6a9f0914c3e909cdde
Jakub Sokołowski <jakub@status.im>
2019-08-07 16:21:43 -0400

    improve build.sh script
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-if [[ "${NODE_ENV}" != "production" ]]; then
+if [[ "${NODE_ENV}" == "production" ]]; then
+    export EMBARK_TARGET=livenet
+    if [[ -z "${WALLET_MNEMONIC}" ]]; then echo "Not defined: WALLET_MNEMONIC"; exit 1; fi
+    if [[ -z "${WALLET_PASSWORD}" ]]; then echo "Not defined: WALLET_PASSWORD"; exit 1; fi
+elif [[ "${NODE_ENV}" == "development" ]]; then
+    export EMBARK_TARGET=testnet
     export WALLET_MNEMONIC='erupt point century seek certain escape solution flee elegant hard please pen'
     export WALLET_PASSWORD='dev_password'
+else
+    echo "Unknown NODE_ENV value: ${NODE_ENV}"
+    echo "Use 'production' or 'development'."
+    exit 1
 fi
 
 echo "removing old full-build"
 rm -rf full-build app.zip
 echo "compiling contracts"
-./node_modules/.bin/embark build
+./node_modules/.bin/embark build "${EMBARK_TARGET}"
 echo "creating new full-build"
 mkdir full-build
 cp -r back-end/* full-build/
