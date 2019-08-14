7215a166b9a9eaae5b5b40b31b60e1e58ed358e5
Richard Ramos <info@richardramos.me>
2019-08-14 13:11:01 -0400

    fix: return promise

--- src/common/blockchain/services/helpers.js
+++ src/common/blockchain/services/helpers.js
@@ -1,15 +1,17 @@
 export const broadcastContractFn = (contractMethod, account) => {
-  return contractMethod
-    .estimateGas({ from: account })
-    .then(estimatedGas => {
-      contractMethod
-        .send({ from: account, gas: estimatedGas + 1000 })
-        .on('transactionHash', hash => {
-          resolve(hash)
-        })
-        .on('error', error => {
-          reject(error)
-        })
-    })
-    .catch(error => reject)
+  return new Promise((resolve, reject) => {
+    contractMethod
+      .estimateGas({ from: account })
+      .then(estimatedGas => {
+        contractMethod
+          .send({ from: account, gas: estimatedGas + 1000 })
+          .on('transactionHash', hash => {
+            resolve(hash)
+          })
+          .on('error', error => {
+            reject(error)
+          })
+      })
+      .catch(error => reject)
+  })
 }
